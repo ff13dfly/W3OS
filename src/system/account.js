bebf22a0b40b0fef74fc4b52b9576373c67afefd
backdoor <backdoor705@proton.me>
2023-11-11 17:50:58 +0800

    refactor using a shared system header

--- src/system/account.js
+++ src/system/account.js
@@ -4,9 +4,9 @@ import { useEffect, useState } from "react";
 import Login from "../components/login";
 import User from "../components/user";
 import RUNTIME from "../lib/runtime";
+import SystemHeader from "./header";
 
 function Account(props) {
-  const size = [3, 6, 3];
   const funs = props.funs;
 
   let [details, setDetails] = useState("");
@@ -39,63 +39,11 @@ function Account(props) {
     RUNTIME.networkStatus("anchor", (res) => {
       //console.log(res);
     });
-  }, []);
+  });
 
   return (
     <div id="page" className={animation}>
-      <Navbar className="bg-body-tertiary">
-        <Container>
-          <Row style={{ width: "100%", margin: "0 auto" }}>
-            <Col
-              xs={size[0]}
-              sm={size[0]}
-              md={size[0]}
-              lg={size[0]}
-              xl={size[0]}
-              xxl={size[0]}
-              style={{ paddingTop: "6px" }}
-            >
-              <Navbar.Brand href="#">
-                W<span className="logo">3</span>OS
-              </Navbar.Brand>
-            </Col>
-            <Col
-              xs={size[1]}
-              sm={size[1]}
-              md={size[1]}
-              lg={size[1]}
-              xl={size[1]}
-              xxl={size[1]}
-              style={{ paddingTop: "10px" }}
-              className="text-center"
-            >
-              Account Setting
-            </Col>
-            <Col
-              xs={size[2]}
-              sm={size[2]}
-              md={size[2]}
-              lg={size[2]}
-              xl={size[2]}
-              xxl={size[2]}
-              className="text-end pb-2"
-              style={{ paddingTop: "10px" }}
-            >
-              <span
-                className="close"
-                onClick={(ev) => {
-                  setAnimation("ani_scale_out");
-                  setTimeout(() => {
-                    props.funs.page("");
-                  }, 300);
-                }}
-              >
-                <button className="btn btn-sm btn-default">X</button>
-              </span>
-            </Col>
-          </Row>
-        </Container>
-      </Navbar>
+      <SystemHeader funs={funs} setAnimation={setAnimation} title="Account Settings" />
       <Container>{details}</Container>
     </div>
   );
