ca2ba011444daa5f192e700f2b86d0e9cf144d85
fuu <fu@yihaofang.com>
2023-12-08 09:51:09 +0800

    Group operation icons selected.

--- src/components/group_opt.js
+++ src/components/group_opt.js
@@ -11,10 +11,21 @@ import { IoIosMore } from "react-icons/io";
 import RUNTIME from "../lib/runtime";
 import Mask from "../layout/mask";
 
+import { BiSolidRename } from "react-icons/bi";
+import { MdOutlineAnnouncement } from "react-icons/md";
+import { IoMdExit } from "react-icons/io";
+import { IoTrashBinOutline } from "react-icons/io5";
+import { MdJoinLeft } from "react-icons/md";
+import { GiDivert } from "react-icons/gi";
+import { FaRegCircleStop } from "react-icons/fa6";
+import { IoMdInformationCircleOutline } from "react-icons/io";
+import { MdUnfoldMore } from "react-icons/md";
+
 function GroupOpt(props) {
   const size = {
     row: [12],
     list: [1, 3, 8],
+    cell:[3],
   };
   const group = props.id;
 
@@ -102,68 +113,77 @@ function GroupOpt(props) {
         <Col hidden={hidden} className="pt-2" xs={size.row[0]} sm={size.row[0]} md={size.row[0]}
           lg={size.row[0]} xl={size.row[0]} xxl={size.row[0]}>
           <Row className="pt-2" style={{ background: "#EEFFEE" }}>
-            <Col className="pb-2" xs={size.row[0]} sm={size.row[0]} md={size.row[0]}
-              lg={size.row[0]} xl={size.row[0]} xxl={size.row[0]} onClick={(ev) => {
+            <Col className="pb-2 pt-2 text-center" xs={size.cell[0]} sm={size.cell[0]} md={size.cell[0]}
+              lg={size.cell[0]} xl={size.cell[0]} xxl={size.cell[0]} onClick={(ev) => {
                 self.clickNick(ev);
                 setHidden(true);
               }}>
-              <button className="btn btn-sm btn-primary">Group Name</button>
+              <BiSolidRename />
+              <p className="pt-2">Name</p>
             </Col>
-            <Col className="pb-2" xs={size.row[0]} sm={size.row[0]} md={size.row[0]}
-              lg={size.row[0]} xl={size.row[0]} xxl={size.row[0]} onClick={(ev) => {
+            <Col className="pb-2 pt-2 text-center" xs={size.cell[0]} sm={size.cell[0]} md={size.cell[0]}
+              lg={size.cell[0]} xl={size.cell[0]} xxl={size.cell[0]} onClick={(ev) => {
                 self.clickAnnounce(ev);
                 setHidden(true);
               }}>
-              <button className="btn btn-sm btn-primary">Announcement</button>
+              <MdOutlineAnnouncement />
+              <p className="pt-2">Announce</p>
             </Col>
-            <Col className="pb-2" xs={size.row[0]} sm={size.row[0]} md={size.row[0]}
-              lg={size.row[0]} xl={size.row[0]} xxl={size.row[0]} onClick={(ev) => {
+            <Col className="pb-2 pt-2 text-center" xs={size.cell[0]} sm={size.cell[0]} md={size.cell[0]}
+              lg={size.cell[0]} xl={size.cell[0]} xxl={size.cell[0]} onClick={(ev) => {
                 self.clickLeave(ev);
                 setHidden(true);
               }}>
-              <button className="btn btn-sm btn-primary">Leave</button>
+              <IoMdExit />
+              <p className="pt-2">Leave</p>
             </Col>
-            <Col className="pb-2" xs={size.row[0]} sm={size.row[0]} md={size.row[0]}
-              lg={size.row[0]} xl={size.row[0]} xxl={size.row[0]} onClick={(ev) => {
+            <Col className="pb-2 pt-2 text-center" xs={size.cell[0]} sm={size.cell[0]} md={size.cell[0]}
+              lg={size.cell[0]} xl={size.cell[0]} xxl={size.cell[0]} onClick={(ev) => {
                 self.clickDestory(ev);
                 setHidden(true);
               }}>
-              <button className="btn btn-sm btn-primary">Destory</button>
+              <IoTrashBinOutline />
+              <p className="pt-2">Destory</p>
             </Col>
-            <Col className="pb-2" xs={size.row[0]} sm={size.row[0]} md={size.row[0]}
-              lg={size.row[0]} xl={size.row[0]} xxl={size.row[0]} onClick={(ev) => {
+            <Col className="pb-2 pt-2 text-center" xs={size.cell[0]} sm={size.cell[0]} md={size.cell[0]}
+              lg={size.cell[0]} xl={size.cell[0]} xxl={size.cell[0]} onClick={(ev) => {
                 self.clickAdd(ev);
                 setHidden(true);
               }}>
-              <button className="btn btn-sm btn-primary">add</button>
+              <MdJoinLeft/>
+              <p className="pt-2">Add</p>
             </Col>
-            <Col className="pb-2" xs={size.row[0]} sm={size.row[0]} md={size.row[0]}
-              lg={size.row[0]} xl={size.row[0]} xxl={size.row[0]} onClick={(ev) => {
+            <Col className="pb-2 pt-2 text-center" xs={size.cell[0]} sm={size.cell[0]} md={size.cell[0]}
+              lg={size.cell[0]} xl={size.cell[0]} xxl={size.cell[0]} onClick={(ev) => {
                 self.clickDivert(ev);
                 setHidden(true);
               }}>
-              <button className="btn btn-sm btn-primary">Divert</button>
+              <GiDivert />
+              <p className="pt-2">Divert</p>
             </Col>
-            <Col className="pb-2" xs={size.row[0]} sm={size.row[0]} md={size.row[0]}
-              lg={size.row[0]} xl={size.row[0]} xxl={size.row[0]} onClick={(ev) => {
+            <Col className="pb-2 pt-2 text-center" xs={size.cell[0]} sm={size.cell[0]} md={size.cell[0]}
+              lg={size.cell[0]} xl={size.cell[0]} xxl={size.cell[0]} onClick={(ev) => {
                 self.clickBlock(ev);
                 setHidden(true);
               }}>
-              <button className="btn btn-sm btn-primary">Block</button>
+              <FaRegCircleStop />
+              <p className="pt-2">Block</p>
             </Col>
-            <Col className="pb-2" xs={size.row[0]} sm={size.row[0]} md={size.row[0]}
-              lg={size.row[0]} xl={size.row[0]} xxl={size.row[0]}>
-              <button className="btn btn-sm btn-primary" onClick={(ev) => {
+            <Col className="pb-2 pt-2 text-center" xs={size.cell[0]} sm={size.cell[0]} md={size.cell[0]}
+              lg={size.cell[0]} xl={size.cell[0]} xxl={size.cell[0]} onClick={(ev) => {
                 self.clickInformation(ev);
                 setHidden(true);
-              }}>Information</button>
+              }}>
+              <IoMdInformationCircleOutline />
+              <p className="pt-2">Info</p>
             </Col>
-            <Col className="pb-2" xs={size.row[0]} sm={size.row[0]} md={size.row[0]}
-              lg={size.row[0]} xl={size.row[0]} xxl={size.row[0]} onClick={(ev) => {
+            <Col className="pb-2 pt-2 text-center" xs={size.cell[0]} sm={size.cell[0]} md={size.cell[0]}
+              lg={size.cell[0]} xl={size.cell[0]} xxl={size.cell[0]} onClick={(ev) => {
                 self.clickMore(ev);
                 setHidden(true);
               }}>
-              <button className="btn btn-sm btn-primary">More</button>
+              <MdUnfoldMore />
+              <p className="pt-2">More</p>
             </Col>
           </Row>
         </Col>
